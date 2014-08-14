5b93849640b2b0424bf14f08918ca4de543eb729
rmhaas2211 <rmhaas2211@gmail.com>
2014-08-14 13:25:21 -0500

    Updated 1
    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,74 @@
 
 $(document).ready(function() {
 	//add/remove exit image to list
-	$('#shoppingList li').mouseenter(function() {
+	$('#shoppingList').on('mouseenter','li',function() {
 		var xout = $('<img class="xout" src="images/xout2.gif">');
 		$(this).append(xout);
 	});
 
-	$('#shoppingList li').mouseleave(function() {
+	$('#shoppingList').on('mouseleave','li',function() {
 		$('#shoppingList li img').remove();
 	});
 
 	//delete li node when exit img is clicked
-	$('#shoppingList li img').on('click', function() {
-		alert('this is working');
-		$(this).closest('li').remove();
+	$("#shoppingList").on("click", "img", function() {
+        $(this).parent().remove();
+	}﻿);
+
+	//show instructions when clicked
+	//hide instructions when exit clicked
+	$('#help').on('click','button', function() {
+		$('#helpInstructions').fadeToggle();
 	});
 
+	$('#helpInstructions').on('click','img',function() { 
+			$('#helpInstructions').fadeOut();
+	});
+
+	//Greys out bought items
+	$('#shoppingList').on('click','li',function() {
+
+		if($(this).attr('class')=="boughtItem") {
+			$(this).removeClass('boughtItem');
+		} else {
+			$(this).addClass('boughtItem');
+		}
+
+
+		//sends clicked list items to bottom/top
+		if ($(this).attr('class')=="boughtItem") {
+			var listItemText=$(this).closest('li').text();
+			$(this).closest('ul').append("<li class='boughtItem'>" + listItemText + "</li>");
+			$(this).remove();	
+		} else {
+			var listItemText=$(this).closest('li').text();
+			$(this).closest('ul').prepend("<li>" + listItemText + "</li>");
+			$(this).remove();
+		}	
+	}); 
+
+	//adds entered items to list using click
+	$('#entry').on('click', 'button', function() {
+		var itemAdd = $('#addItem').val();
+		if (itemAdd == "") {
+			alert("Please enter an item");
+		} else {
+			$('#shoppingList ul').prepend('<li>' + itemAdd + '</li>');
+			$('#addItem').val('');
+		}
+	});
+
+	//adds entered items to list using enter
+	$('#entry').on('keyup','input', function(event) {
+		var itemAdd = $('#addItem').val();
+		if(event.keyCode == "13") {
+			if (itemAdd == "") {
+			alert("Please enter an item");
+			} else {
+				$('#shoppingList ul').prepend('<li>' + itemAdd + '</li>');
+				$('#addItem').val('');
+			}
+		}
+	});
 
 });
